102454e1688f9ae65074225a4f9587bf8868c3d4
Angelika Bauerová <angelika.bauerova@gmail.com>
2023-04-10 15:21:06 +0200

    uprava policek a animace

---
+++ hra.js
@@ -0,0 +1,48 @@
+let currentPlayer = 'circle';
+
+const selectButton = (event) => {
+  if (currentPlayer === 'circle') {
+    currentPlayer = 'cross';
+    event.target.classList.add('board__field--circle');
+    event.target.disabled = true;
+    document.getElementById('player').classList.remove('circle');
+    document.getElementById('player').classList.add('cross');
+  } else if (currentPlayer === 'cross') {
+    currentPlayer = 'circle';
+    event.target.classList.add('board__field--cross');
+    document.getElementById('player').classList.remove('cross');
+    document.getElementById('player').classList.add('circle');
+    event.target.disabled = true;
+  }
+};
+
+document
+  .querySelector('button:nth-child(1)')
+  .addEventListener('click', selectButton);
+document
+  .querySelector('button:nth-child(2)')
+  .addEventListener('click', selectButton);
+document
+  .querySelector('button:nth-child(3)')
+  .addEventListener('click', selectButton);
+document
+  .querySelector('button:nth-child(4)')
+  .addEventListener('click', selectButton);
+document
+  .querySelector('button:nth-child(5)')
+  .addEventListener('click', selectButton);
+document
+  .querySelector('button:nth-child(6)')
+  .addEventListener('click', selectButton);
+document
+  .querySelector('button:nth-child(7)')
+  .addEventListener('click', selectButton);
+document
+  .querySelector('button:nth-child(8)')
+  .addEventListener('click', selectButton);
+document
+  .querySelector('button:nth-child(9)')
+  .addEventListener('click', selectButton);
+document
+  .querySelector('button:nth-child(10)')
+  .addEventListener('click', selectButton);
