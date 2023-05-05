b1f79498954a012725634f7827fe7e3f7e5b8e54
Angelika Bauerová <angelika.bauerova@gmail.com>
2023-05-05 09:33:07 +0200

    uprava funkci na hru s PC a prehazeni kodu

--- hra.js
+++ hra.js
@@ -1,25 +1,9 @@
 import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
-let currentPlayer = 'circle';
-
-const selectButton = (event) => {
-  if (currentPlayer === 'circle') {
-    currentPlayer = 'cross';
-    event.target.classList.add('board__field--circle');
-    event.target.disabled = true;
-    document.getElementById('player').classList.remove('circle');
-    document.getElementById('player').classList.add('cross');
-  } else if (currentPlayer === 'cross') {
-    currentPlayer = 'circle';
-    event.target.classList.add('board__field--cross');
-    document.getElementById('player').classList.remove('cross');
-    document.getElementById('player').classList.add('circle');
-    event.target.disabled = true;
-  }
-  //Turning buttons into symbols array
-  const herniPole = document.querySelectorAll('.playBtn');
-  const herniPoleArray = Array.from(herniPole);
-  const herniSymboly = herniPoleArray.map((symbol) => {
+//Turning buttons into symbols array
+const herniPole = document.querySelectorAll('.playBtn');
+const herniPoleArray = () => {
+  return Array.from(herniPole).map((symbol) => {
     if (symbol.classList.contains('board__field--cross')) {
       return 'x';
     } else if (symbol.classList.contains('board__field--circle')) {
@@ -28,8 +12,11 @@ const selectButton = (event) => {
       return '_';
     }
   });
-  //Declaring winner
-  const vitez = findWinner(herniSymboly);
+};
+
+//Declaring winner - function in function, so I can use it below in selectButton()
+const vitezFunc = () => {
+  const vitez = findWinner(herniPoleArray());
   if (vitez === 'o') {
     setTimeout(() => {
       alert('Vyhrálo kolečko!');
@@ -47,6 +34,65 @@ const selectButton = (event) => {
     }, 200);
   }
 };
+
+//Function for playing with PC - defining const buttons, fetch data and returning function as part of selectButton() below
+const buttons = document.querySelectorAll('.playBtn');
+
+const pocitacHraje = () => {
+  currentPlayer = 'cross';
+  buttons.forEach((button) => {
+    button.disabled = true;
+  });
+  fetch('https://piskvorky.czechitas-podklady.cz/api/suggest-next-move', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      board: herniPoleArray(),
+      player: 'x',
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const { x, y } = data.position;
+      const index = x + y * 10;
+      buttons.forEach((button) => {
+        if (
+          button.classList.contains('board__field--cross') ||
+          button.classList.contains('board__field--circle')
+        ) {
+          button.disabled = true;
+        } else {
+          button.disabled = false;
+        }
+      });
+      buttons[index].click();
+    });
+};
+
+let currentPlayer = 'circle';
+
+const selectButton = (event) => {
+  if (currentPlayer === 'circle') {
+    currentPlayer = 'cross';
+    event.target.classList.add('board__field--circle');
+    event.target.disabled = true;
+    document.getElementById('player').classList.remove('circle');
+    document.getElementById('player').classList.add('cross');
+    vitezFunc();
+    return pocitacHraje();
+  } else if (currentPlayer === 'cross') {
+    event.target.classList.add('board__field--cross');
+    document.getElementById('player').classList.remove('cross');
+    document.getElementById('player').classList.add('circle');
+    event.target.disabled = true;
+    vitezFunc();
+    return (currentPlayer = 'circle');
+  }
+};
+
+//Window refresh
 const restart = (event) => {
   if (window.confirm('Opravdu chceš začít znovu?')) {
     location.reload();
